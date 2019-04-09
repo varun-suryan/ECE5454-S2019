505af9d902a6c897ab25f4b3976824d54606f9fb
Varun Suryan <varunsuryan9@gmail.com>
2019-04-09 11:04:23 -0400

    HW3 prob3 done

---
+++ prob3.m
@@ -0,0 +1,13 @@
+clear all; close all; clc;
+
+% normalize the dataset
+X = normalize(csvread('datamatrix.csv', 1, 1)');
+
+% Calculate the svd here
+[U, S, ~] = svd((X'*X)/size(X, 1));
+
+%project onto two dimensions
+projected_data = X*U(:, 1:2);
+
+% recover the original data again by projecting back
+X_recovered = projected_data*U(:, 1:2)';
